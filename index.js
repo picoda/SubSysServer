1da4dc029265ce6edd10c3b2195eb787235da955
Agah Ebrahimi <Agah@Agahs-MBP.fios-router.home>
2019-05-03 01:40:32 -0400

    a

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const cors = require('cors');
+const bodyParser = require("body-parser");
+const controller = require('./server/controller');
 
 const expressApp = express();
 
+//expressApp.use(express.json());
+expressApp.use(bodyParser({limit: '50mb'}));
 expressApp.use(cors());
+expressApp.use(controller);
 
 expressApp.listen(8080);
---
+++ server/upload.js
@@ -0,0 +1,9 @@
+var express = require('express'), router = express.Router();
+ 
+router.post("/submit", function(req, res){
+    res.end();
+});
+
+
+module.exports = router
+
