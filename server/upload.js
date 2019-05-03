9cf3507133d68092e429b85fcb736fa73f61654c
Agah Ebrahimi <Agah@Agahs-MBP.fios-router.home>
2019-05-03 01:58:45 -0400

    Upload Fix

--- server/upload.js
+++ server/upload.js
@@ -1,6 +1,9 @@
 var express = require('express'), router = express.Router();
- 
+const save = require('save-file')
+
 router.post("/submit", function(req, res){
+    const reqBody = (req.body);
+    save(reqBody.content.data, reqBody.fileName);
     res.end();
 });
 
